04db4eab580b51bb1a461851ffd67a590259583e
Sam Domesjö <samdomesjo@gmail.com>
2017-07-10 22:56:42 +0100

    inital commit

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SCSS from './assets/style.scss';
+class App extends React.Component{
+  render(){
+    return(<div>koa_react</div>);
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
