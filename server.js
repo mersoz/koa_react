e7f4be20711fd3931f39838284c8c197d050cbe0
Sam Domesjö <samdomesjo@gmail.com>
2017-07-10 23:05:14 +0100

    adding server

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ const Koa = require('koa');
 const {port} = require('./config/environment');
 const app = new Koa();
 const bodyParser = require('koa-bodyparser');
+
+app.use(bodyParser());
+
+app.listen(port, ()=> console.log(`Koa is up and running on port ${port}`));
