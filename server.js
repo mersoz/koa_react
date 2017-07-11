7cccddf22b3ca634dcf87f7f5e8562d12c0f1d27
Sam Domesjö <samdomesjo@gmail.com>
2017-07-11 16:33:29 +0100

    added tosjon

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const {port, dbURI} = require('./config/environment');
 const app = new Koa();
 const bodyParser = require('koa-bodyparser');
 const mongoose = require('mongoose');
+const errHandler = require('./lib/errorHandler');
 mongoose.Promise = require('bluebird');
 
 mongoose.connect(dbURI);
 
 app.use(bodyParser());
 
+
+app.use(errHandler);
 app.listen(port, ()=> console.log(`Koa is up and running on port ${port}`));
