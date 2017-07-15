995ded2f5428efe50ebda5c2a3bc47193c0f3eed
Sam Domesjö <samdomesjo@gmail.com>
2017-07-15 15:00:13 +0100

    schema created

--- models/user.js
+++ models/user.js
@@ -18,3 +18,17 @@ userSchema.pre('validate', function checkPasswordConfirmation(next){
   }
   next();
 });
+
+userSchema.pre('save', function hashPassword(next){
+  if(this.isModified('password')) {
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
+  }
+  next();
+});
+
+userSchema.methods.validatePassword = function validatePassword(password){
+  if(!password) return false;
+  return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
