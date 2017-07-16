b3bc0e00c9a8e385d1ce88d13cb034328abac0eb
Sam Domesjö <samdomesjo@gmail.com>
2017-07-16 17:27:49 +0100

    returns a status

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,10 @@
 const router = new require('koa-router')();
+const auth = require('../controllers/auth');
 
 router.get('/api/', (ctx)=>{
   ctx.body = 'hello world';
 });
+
+router.post('/api/register', auth.register);
 
 module.exports = router;
--- controllers/auth.js
+++ controllers/auth.js
@@ -2,8 +2,11 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const { secret } = require('../config/environment');
 
-export default class Auth {
+class Auth {
   async register(ctx){
-    console.log(ctx);
+    ctx.body = await User.create(ctx.request.body);
+    ctx.response.status= 201;
   }
 }
+const auth = new Auth;
+module.exports = auth;
