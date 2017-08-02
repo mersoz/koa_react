95a58d7e77a1e6948194d4f6be45b9225e24fcd6
Sam Domesjö <samdomesjo@gmail.com>
2017-08-03 00:34:42 +0100

    refactor

--- app/components/user/visualUser.js
+++ app/components/user/visualUser.js
@@ -4,12 +4,12 @@ import {Link} from 'react-router-dom';
 export default class VisualUser extends Component {
   render() {
     return(
-      <div>
-        <h1>{ this.props.user ? this.props.user.username:''}</h1>
-        <p><strong>emal: { this.props.user ? this.props.user.email:''}</strong></p>
-        <Link to={`/profile/${this.props.user ? this.props.user._id: ''}/edit`}><button>Edit</button></Link>
+      this.props.user ? ( <div>
+        <h1>{this.props.user.username}</h1>
+        <p><strong>emal: {this.props.user.email}</strong></p>
+        <Link to={`/profile/${this.props.user._id}/edit`}><button>Edit</button></Link>
         <button disabled onClick={this.props.removeUser}>Delete</button>
-      </div>
+      </div> ): (<div>Loading..</div>)
     );
   }
 }
