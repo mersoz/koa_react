3df6a83f8f52cf546bccd252ffd3a0526af25b7e
Sam Domesjö <samdomesjo@gmail.com>
2017-08-06 18:08:33 +0100

    added register form

--- app/components/register/register.js
+++ app/components/register/register.js
@@ -1,16 +1,20 @@
 import React, {Component} from 'react';
-
+import $ from 'jquery';
+import VisualRegister from './VisualRegister';
 export default class Register extends Component {
   constructor() {
     super();
     this.state = {};
+    this.setUser = this.setUser.bind(this);
+  }
+
+  setUser(e) {
+    this.setState({ user: {[e.target.name]: e.target.value}}, ()=> console.log(this.state));
   }
 
   render() {
     return(
-      <div>
-      register
-      </div>
+      <VisualRegister setUser={this.setUser} />
     );
   }
 }
---
+++ app/components/register/visualRegister.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+export default class VisualRegister extends Component{
+  render() {
+    return(
+      this.props.setUser ?
+      <div>
+        <h1> Register</h1>
+        <form>
+          <label htmlFor="username">username</label>
+          <input onChange={this.props.setUser} type="text" name="username" placeholder="Demi" />
+          <label htmlFor="email">email</label>
+          <input onChange={this.props.setUser} type="text" name="email" placeholder="DemiLolo@cool.yes" />
+          <label htmlFor="password">password</label>
+          <input onChange={this.props.setUser} type="password" name="password" placeholder="password" />
+          <label htmlFor="passwordConfirmation">passwordConfirmation</label>
+          <input onChange={this.props.setUser} type="password" name="passwordConfirmation" placeholder="Demi" />
+          <button>Join us!</button>
+        </form>
+      </div> : <div> loading ..</div>
+    );
+  }
+}
